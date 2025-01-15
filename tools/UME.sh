8d55491e5465dcd8c71d93e169c5beab37085db0
mifongjvav <156570698+mifongjvav@users.noreply.github.com>
2025-01-15 16:12:53 +0800

    Add files via upload

--- tools/UME.sh
+++ tools/UME.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
 echo "欢迎使用Uninstall ME!"
-# 询问目标目录
-read -p "请输入目标目录（输入/0表示当前目录）: " target_dir
 
-# 如果输入为/0，则设置为当前目录
-if [ "$target_dir" == "/0" ]; then
-    target_dir=$(pwd)
-fi
+# 获取脚本所在的目录和文件名
+SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
+SCRIPT_NAME=$(basename "$0")
+
+# 询问目标目录
+read -p "请输入目标目录: " target_dir
 
 # 检查目录是否存在
 if [ ! -d "$target_dir" ]; then
     echo "目录不存在，请重新运行脚本并输入有效的目录。"
+    exit 1
+fi
+
+# 检查目标目录是否是脚本所在目录
+if [ "$target_dir" == "$SCRIPT_DIR" ]; then
+    echo "错误：不能删除脚本所在的目录。"
     exit 1
 fi
 
@@ -24,8 +30,8 @@ read -p "请再次确认 (1: 是, 0: 否): " confirm2
 
 # 检查两次确认
 if [ "$confirm1" == "1" ] && [ "$confirm2" == "1" ]; then
-    # 查找并删除文件
-    find "$target_dir" -type f -name "*.$file_extension" -exec rm -v {} \;
+    # 查找并删除文件，排除脚本自身和其他 Bash 脚本
+    find "$target_dir" -type f -name "*.$file_extension" ! -path "$SCRIPT_DIR/*" ! -name "$SCRIPT_NAME" ! -name "*.sh" -exec rm -v {} \;
     echo "文件删除完成。"
 else
     echo "操作已取消。"
